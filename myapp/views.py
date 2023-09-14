ec95f532a320de7d9b82b6ace51aad01deaca331
Tec94 <caodangkhiet1@gmail.com>
2023-09-14 03:25:22 -0500

    add function that returns the url link for movie cover add function that cleans the url link to fullsize return movie title and cover url to the html

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Movie
+from imdb import Cinemagoer
+import os
 
 # Create your views here.
+def url_clean(url):
+    base, ext = os.path.splitext(url)
+    i = url.count('@')
+    s2 = url.split('@')[0]
+    url = s2 + '@' * i + ext
+    return url
+
 def home(request):
-    return render(request, 'home.html', {})
+    ia = Cinemagoer()
+    ia.get_movie_infoset()
+    movie = ia.search_movie("Matrix")
+    movie = movie[0]
+    url = url_clean(movie['cover url'])
+    photo = url
+    return render(request, 'home.html', {'movie': movie, 'photo': photo})
