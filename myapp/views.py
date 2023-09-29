bd0d2b4f02c1688a9e44a673fb8be51b5d15520b
Tec94 <caodangkhiet1@gmail.com>
2023-09-29 12:57:40 -0500

    use seperate movie poster variables for each movie add movie poster randomizer functionality (might remove later on)

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.db import connections
 from .models import Movie
 from imdb import Cinemagoer
-import os
+import imdb
+import os, random
 
 # Create your views here.
 def url_clean(url):
@@ -12,10 +14,56 @@ def url_clean(url):
     return url
 
 def home(request):
-    ia = Cinemagoer()
+    ia = Cinemagoer('s3', 'mysql+mysqldb://root:root@localhost:3306/moviesDB') # local dataset - no cover url
+    ib = imdb.IMDb() # imdb database - cover url
     ia.get_movie_infoset()
-    movie = ia.search_movie("Matrix")
-    movie = movie[0]
-    url = url_clean(movie['cover url'])
-    photo = url
-    return render(request, 'home.html', {'movie': movie, 'photo': photo})
+    ib.get_movie_infoset()
+
+    movie1 = ia.get_movie(random.randint(0, 999999))
+    movie2 = ia.get_movie(random.randint(0, 999999))
+    movie3 = ia.get_movie(random.randint(0, 999999))
+    movie4 = ia.get_movie(random.randint(0, 999999))
+    movie5 = ia.get_movie(random.randint(0, 999999))
+    movie6 = ia.get_movie(random.randint(0, 999999))
+
+    # if keyerror raised then set a blank cover url
+    try:
+        mv1_cover = ib.search_movie(movie1['title'])[0].data['cover url']
+    except KeyError:
+        mv1_cover = ''
+    try:
+        mv2_cover = ib.search_movie(movie2['title'])[0].data['cover url']
+    except KeyError:
+        mv2_cover = ''
+    try:
+        mv3_cover = ib.search_movie(movie3['title'])[0].data['cover url']
+    except KeyError:
+        mv3_cover = ''
+    try:
+        mv4_cover = ib.search_movie(movie4['title'])[0].data['cover url']
+    except KeyError:
+        mv4_cover = ''
+    try:
+        mv5_cover = ib.search_movie(movie5['title'])[0].data['cover url']
+    except KeyError:
+        mv5_cover = ''
+    try:
+        mv6_cover = ib.search_movie(movie6['title'])[0].data['cover url']
+    except KeyError:
+        mv6_cover = ''
+
+    url1 = url_clean(mv1_cover)
+    url2 = url_clean(mv2_cover)
+    url3 = url_clean(mv3_cover)
+    url4 = url_clean(mv4_cover)
+    url5 = url_clean(mv5_cover)
+    url6 = url_clean(mv6_cover)
+
+    photo1 = url1
+    photo2 = url2
+    photo3 = url3
+    photo4 = url4
+    photo5 = url5
+    photo6 = url6
+
+    return render(request, 'home.html', {'movie1': movie1, 'movie2': movie2, 'movie3': movie3, 'movie4': movie4, 'movie5': movie5, 'movie6': movie6, 'photo1': photo1, 'photo2': photo2, 'photo3': photo3, 'photo4': photo4, 'photo5': photo5, 'photo6': photo6})
