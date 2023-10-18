27537530d37907c6e746c6c08d81a95821559cf5
Tec94 <caodangkhiet1@gmail.com>
2023-10-17 22:41:31 -0500

    remove uneeded code

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from imdb import Cinemagoer
 
 # Create your models here.
-class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    poster = models.CharField(max_length=500)
-
-    def get_movie_data(self, id):
-        return Cinemagoer.get_movie(id)
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
