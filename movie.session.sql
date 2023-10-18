6a8fb45ac87e16efd8ef32f0ef5aa96ac5583e62
Tec94 <caodangkhiet1@gmail.com>
2023-10-17 22:40:37 -0500

    update gitignore add table User in sql db

--- movie.session.sql
+++ movie.session.sql
@@ -1,3 +1,6 @@
-{
-    
-}
+CREATE TABLE "myapp_user" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "name" varchar(100) NOT NULL,
+    "password" varchar(100) NOT NULL,
+    "email" varchar(100) NOT NULL
+);
