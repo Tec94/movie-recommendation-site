8ca929bf172429564e24293e40a4fa111dd794af
Tec94 <caodangkhiet1@gmail.com>
2023-11-01 21:32:41 -0500

    delete User models to use built-in User model

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,3 @@ from django.db import models
 from imdb import Cinemagoer
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
